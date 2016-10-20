2e62facc7f48d2bafa3e0a39e78a6ccc4123f84b
Stefan <hoitaus@gmail.com>
2016-10-20 17:25:30 +0800

    restructure

--- elephancy.go
+++ elephancy.go
@@ -1,51 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
-	"io/ioutil"
 	"net/http"
-	"os"
 	"regexp"
-	"sort"
 	"time"
 )
 
 var pathFavicon string = "staticcache/favicon.ico"
-
-func readContent(filename string) ([]byte, time.Time, error) {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, time.Time{}, err //cannot use nil as type time.Time in return argument
-	}
-	// this is also done in ReadFile...inefficient
-	// no need to check for error, since already done by ReadFile
-	f, err := os.Open(filename)
-	defer f.Close()
-	fi, err := f.Stat()
-	if err != nil {
-		return nil, time.Time{}, err
-	}
-	return content, fi.ModTime(), err
-}
-
-// loadPages opens a json file and returns the contents as a map[string]interface{}
-// TODO: handle errors
-func loadPages() (map[string]interface{}, error) {
-
-	filename := "./json/pages.json"
-	bytearr, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	var p interface{}
-	err = json.Unmarshal(bytearr, &p)
-	if err != nil {
-		return nil, err
-	}
-	m := p.(map[string]interface{})
-	return m, nil
-}
 
 // this is modified from package http
 func ifNotModifiedResponse(w http.ResponseWriter, r *http.Request, modtime time.Time) bool {
@@ -72,82 +34,19 @@ var contentPath = regexp.MustCompile("^/content/([a-zA-Z0-9]+).html$")
 
 var templates = template.Must(template.ParseFiles("./templ/frame.html"))
 
-// Urlpath2pagename returns the pagename of a requested url
-func Urlpath2pagename(urlpath string, m map[string]interface{}) string {
-	// TODO: return error in case it doesn't find anything
-	for pagename, page := range m {
-		pag := page.(map[string]interface{})
-		if urlpath == pag["Urlpath"].(string) {
-			return pagename
-		}
-	}
-	return ""
-}
-
-//TODO: abstract this and Urlpath2pagename to one function
-
-func Contenturl2pagename(contenturl string, m map[string]interface{}) string {
-	// TODO: return error in case it doesn't find anything
-	for pagename, page := range m {
-		pag := page.(map[string]interface{})
-		if contenturl == "/"+pag["ContentUrl"].(string) {
-			return pagename
-		}
-	}
-	return ""
-}
-
-// map2array orders page data into an array according linkweights. Itakes a map[string]interface{}, which maps pagenames to pages. A page is a json object containing page data as unordered list of key:value pairs. It returns an array of these objects, which is sorted corresponding to the "Linkweight" keys of the page objects.
-func map2array(m map[string]interface{}) (arr []map[string]interface{}) {
-	// we need an array for iteration order. see http://blog.golang.org/go-maps-in-action
-	g := make(map[int]string)
-	for pname, page := range m {
-		pag := page.(map[string]interface{})
-		g[int(pag["Linkweight"].(float64))] = pname
-	}
-
-	var keys []int
-	for k := range g {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-
-	for _, k := range keys {
-		arr = append(arr, m[g[k]].(map[string]interface{}))
-	}
-	return arr
-}
-
 func pagesHandler(w http.ResponseWriter, r *http.Request) {
 
-	m, err := loadPages()
-	if err != nil {
-		return
-	}
-
-	// look up which page (pagename) is associated with the requested url
-	pagename := Urlpath2pagename(r.URL.Path, m)
-	if pagename == "" {
-		http.NotFound(w, r)
-		return
-	}
-	// load the page data into a map[string]interface{}
-	page := m[pagename].(map[string]interface{})
-	// read content from html file and get modification time
-	content, modtime, err := readContent(page["ContentUrl"].(string))
-
+	templdat, modtime, err := getTemplateData(r.URL.Path)
 	// TODO: check if the frame as been modified and set modtime to the more recent time. Maybe no need to read frame.html since we use template caching. Is it possible to compare to the the time of compilation? Or maybe better time since server is started?
 	if ifNotModifiedResponse(w, r, modtime) {
 		return
 	}
+	err = templates.ExecuteTemplate(w, "frame.html", &templdat)
 
-	arr := map2array(m)
-	blab := make(map[string]interface{})
-	blab["Pages"] = arr
-	blab["Content"] = template.HTML(string(content))
-	blab["Metatitle"] = page["Metatitle"].(string)
-	err = templates.ExecuteTemplate(w, "frame.html", &blab)
-
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 	if err != nil {
 		return
 	}
@@ -194,15 +93,15 @@ func contentHandler(w http.ResponseWriter, r *http.Request) {
 	if ajax == "XMLHttpRequest" {
 		http.FileServer(http.Dir("./")).ServeHTTP(w, r)
 	} else {
-		// translate ContentUrl to Urlpath and redirect
-		m, err := loadPages()
+		// TODO: move this out of this file
+		pcoll, err := loadPages()
 		if err != nil {
 			return
 		}
 		// fill in content
-		path := r.URL.Path
-		pagename := Contenturl2pagename(path, m)
-		page := m[pagename].(map[string]interface{})
+		contentURL := r.URL.Path
+		pagename := pcoll.contentURLToPagename(contentURL)
+		page := pcoll.getPage(pagename)
 		urlpath := page["Urlpath"].(string)
 		http.Redirect(w, r, urlpath, 302)
 	}
---
+++ myjson.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"sort"
+	"time"
+)
+
+type pagesCollection map[string]interface{}
+type page map[string]interface{}
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
+
+// loadPages opens a json file and returns the contents as a map[string]interface{}
+// TODO: handle errors
+func loadPages() (pagesCollection, error) {
+
+	filename := "./json/pages.json"
+	bytearr, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var p interface{}
+	err = json.Unmarshal(bytearr, &p)
+	if err != nil {
+		return nil, err
+	}
+	m := p.(map[string]interface{})
+	return m, nil
+}
+
+// map2array orders page data into an array according linkweights. It takes a map[string]interface{}, which maps pagenames to pages. A page is a json object containing page data as unordered list of key:value pairs. It returns an array of these objects, which is sorted corresponding to the "Linkweight" keys of the page objects.
+func (pcoll *pagesCollection) toArray() (arr []map[string]interface{}) {
+	// we need an array for iteration order. see http://blog.golang.org/go-maps-in-action
+	g := make(map[int]string)
+	for pname, page := range *pcoll {
+		pag := page.(map[string]interface{})
+		g[int(pag["Linkweight"].(float64))] = pname
+	}
+
+	var keys []int
+	for k := range g {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		arr = append(arr, (*pcoll)[g[k]].(map[string]interface{}))
+	}
+	return arr
+}
+
+// TODO: urlPathToPagename and contentURLToPagename almost same
+
+func (pcoll *pagesCollection) urlPathToPagename(urlpath string) string {
+	// TODO: return error in case it doesn't find anything
+	for pagename, page := range *pcoll {
+		pag := page.(map[string]interface{})
+		if urlpath == pag["Urlpath"].(string) {
+			return pagename
+		}
+	}
+	return ""
+}
+
+func (pcoll *pagesCollection) contentURLToPagename(contenturl string) string {
+	// TODO: return error in case it doesn't find anything
+	for pagename, page := range *pcoll {
+		pag := page.(map[string]interface{})
+		if contenturl == pag["ContentUrl"].(string) {
+			return pagename
+		}
+	}
+	return ""
+}
+
+func (pcoll *pagesCollection) getPage(pagename string) (pg page) {
+	// load the page data into a map[string]interface{}
+	return (*pcoll)[pagename].(map[string]interface{})
+}
+
+func (pcoll *pagesCollection) contentFromPage(pg page) (content []byte, modtime time.Time, err error) {
+	// read content from html file and get modification time
+	content, modtime, err = readContent(pg["ContentUrl"].(string))
+	return content, modtime, err
+}
+
+func getTemplateData(urlpath string) (map[string]interface{}, time.Time, error) {
+	pcoll, err := loadPages()
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+	pagename := pcoll.urlPathToPagename(urlpath)
+	page := pcoll.getPage(pagename)
+	content, modtime, err := pcoll.contentFromPage(page)
+	arr := pcoll.toArray()
+	blab := make(map[string]interface{})
+	blab["Pages"] = arr
+	blab["Content"] = template.HTML(string(content))
+	blab["Metatitle"] = page["Metatitle"].(string)
+	return blab, modtime, err
+}
+
+func readContent(filename string) ([]byte, time.Time, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, time.Time{}, err //cannot use nil as type time.Time in return argument
+	}
+	// this is also done in ReadFile...inefficient
+	// no need to check for error, since already done by ReadFile
+	f, err := os.Open(filename)
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+	return content, fi.ModTime(), err
+}
